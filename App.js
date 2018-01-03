8537792f0e5fcbac393d544fcc6bc215be9afa2d
ak0v <andrejko@gbg.bg>
2018-01-03 17:05:46 +0200

    firebase hosting attached

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ class App extends React.Component {
    render() {
       return (
          <div>
-            Hello World ReactJS!!!
+            Hello World 
+            !!!
          </div>
       );
    }
